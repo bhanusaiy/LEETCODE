2a9d1e343472578e2296835fee3510d7dc009920
bhanusaiy <bhanusaiyarasi23@gmail.com>
2026-01-24 21:25:14 +0530

    Time: 6 ms (49.38%) | Memory: 55.4 MB (67.64%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0,i=0,n=0,count1=0;
+        Arrays.sort(nums);
+            return nums[nums.length/2];
+
+        
+    }
+}
